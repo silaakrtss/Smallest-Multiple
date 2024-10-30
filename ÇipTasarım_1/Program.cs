f5d025a614cbd1ef7e40a7afff06aa5e6054adc0
silaakrtss <sila.krts.68@gmail.com>
2024-10-30 13:02:30 +0300

    first commit

---
+++ ÇipTasarım_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÇipTasarım_1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isDivisible(int num)
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    if (num % i != 0)
+                    {
+                        return false; 
+                    }
+                }
+                return true; 
+            }
+
+            int findSmallestMultiple()
+            {
+                int num = 20; 
+                while (true)
+                {
+                    if (isDivisible(num))
+                    {
+                        return num; 
+                    }
+                    num += 20; 
+                }
+            }
+
+
+            {
+                int result = findSmallestMultiple();
+                Console.WriteLine("1 ile 20 arasındaki tüm sayılara tam bölünebilen en küçük sayı: "+ result);
+                Console.Read();
+            }
+        }
+    }
+}
+
